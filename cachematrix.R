c0b8ecbdfc78524999c3a84994ee6266da5b44f9
junky <junkathing@users.noreply.github.com>
2016-05-12 13:48:28 -1000

    changed the documentation
    changed the documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,13 @@
-#  cachematrix.R
+#  cacheInverse.R
 #  Coursera_Data_Science
 #
-#  Created by CEJ on 8/20/15.
-#  Copyright (c) 2015 CEJ. All rights reserved.
+#  Created by Chad Junkermeier on 5/12/16.
+#  Copyright © 2016 Chad Junkermeier. All rights reserved.
 
 
 
-## Put comments here that give an overall description of what your
-## functions do
 
-## This function that wraps a matrix into a list.  This list contains containing a function to
+## makeCacheMatrix wraps a matrix into a list.  This list contains containing a function to
 ##       1. set the value of the matrix
 ##       2. get the value of the matrix
 ##       3. set the value of the inverse matrix
@@ -31,10 +29,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve is a extension of the normal solve function which "keeps a 
-## cache of the mapping from arguments to results and, when calls with the
-## same arguments are repeated often, has higher performance at the expense
-## of higher memory use." [quote from: http://clojuredocs.org/search?q=memoize.]
+## cacheSolve is a extension of the normal solve function which "keeps a cache of the mapping 
+## from arguments to results and, when calls with the same arguments are repeated often, has 
+## higher performance at the expense of higher memory use." 
+## [quote from: http://clojuredocs.org/search?q=memoize.]
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -46,7 +44,7 @@ cacheSolve <- function(x, ...) {
         data <- x$get()
         m <- solve(data, ...)
         x$setsolve(m)
-        m
+        return(m)
 
 }
 
